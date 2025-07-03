a56d01082a60ac06e51d10919616846ec5d0d1e2
Tmplinkz <139988794+Tmplinkz@users.noreply.github.com>
2025-07-03 11:18:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")
-OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+API_ID = int(os.environ.get("API_ID", "21261146"))
+API_HASH = os.environ.get("API_HASH", "d3b7a147e58bbc6856d6aec9cddb74fc")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6390630887:AAHb3pWdAHCiht1Ftm60ka9QQsGtAGZ3O88")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://ganesh:8525@cluster0.bmgqo8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5066042764"))
 SESSION_STRING = os.environ.get("SESSION_STRING")
