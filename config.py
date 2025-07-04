e8d8ddd61f4cd8f3659a8c7d20110ed9c5e349bb
Tmplinkz <139988794+Tmplinkz@users.noreply.github.com>
2025-07-04 23:02:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", "21261146"))
 API_HASH = os.getenv("API_HASH", "d3b7a147e58bbc6856d6aec9cddb74fc")
-SESSION_STRING = os.getenv("SESSION_STRING", "xBQFEa1oABKEQnHkrL_KzrqRyvhbV5dC49kcIAXHKYktqua6-MGHytFEFxwJOPCHemKyvYEOxj61iWYikiMJYkBA6gmfTgZMA-zWQG_-sgGBz9QrWG8gYX-t_vcaP0QOEINOIRa3RExL4kT-3m1Wo4lhfjwNJ5129GbNyylfN4ECMhjU0rJy2UOJq_ILvR9COJFsBRdPiuzNLZalsFlf744UkKWWay-ZMGaqtyqh-iI1VZkhv7Hi4b8uVNbv16TpZH7pOR-Bgxlw-RquBKgFDzDfh_QtPbBHBqSdshSDNL174Y11zfNodW44mlfYuMfqsBWhJjGdElRmvshaASE1cDB08zYGLVAAAAAHbKHGiAA")
+SESSION_STRING = os.getenv("SESSION_STRING", "BQFEa1oAJRKqGFYiiYObit6AszTajczXUJoTxWX_bmrYNtD7XbPhsyAHF0kzjEdOsqjmR_1SYX2DU-wq0agQJiooQtjRADABoFiRyzwMKbAxbblGyec-aatgakUxTzv4ZktMiUWqxKMkuKH3YNzuTEj3OLtZyPi2VoHrYl_dTOfESqFqJa1642JX7t_CS0XeGhYy-a1CdNHH2GF4oNBnbJgS5XJc1lHWhMPKJYZsEz-opIIWI-2yAk8Meo3gvwijSqauKDbijajgEBU0ilSHtJhzRMsap4MKi4PvtOyWKGyeLDaMCujpUZ6zXS2_hkFgin4kBQBDKUv9O2TIAs3LWw8qYYpviQAAAAHbKHGiAA")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ganesh:8525@cluster0.bmgqo8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.getenv("OWNER_ID", "5066042764"))
 
